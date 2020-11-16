4b1d15edf9cd7ac87a486487d7939364edd3a8ab
Zach Izzard <izzard@usc.edu>
2020-11-15 21:51:34 -0800

    MAYBE NOW!?!?!?!

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: npm run server-prod
+web: node server/server.js
--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
-const port = 8080;
+const port = process.env.PORT || 8080;
 
 app.use(cors());
 
